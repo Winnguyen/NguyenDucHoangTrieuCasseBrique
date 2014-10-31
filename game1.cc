86e946513b46b1959d610c851c8508f752152f00
Winnguyen <winnguyenno1@gmail.com>
2014-10-30 23:45:27 -0700

    update

--- game1.cc
+++ game1.cc
@@ -1,4 +1,3 @@
-/******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,56 +8,175 @@ using namespace std;
 
 /* retourne 0 si aucune touche n'est appuyée, 
  * sinon retourne le code ASCII de la touche */
-int read_keybord();
+ int read_keybord();
 
 /******************************************************************************/
-#define H 20
-#define L 30
-
+#define H 30 // Hauteur
+#define L 40 // Largeur
 char screen[H][L];
+int c=1; int bg=15, bd=25;
+void screen_display()
+{
+  system("clear");
+  for (int i=0; i<H; i++)
+    for (int j=0; j<L; j++)
+      if (i==0||i==H-1||j==0||j==L-1)
+	screen[i][j]='|';
 
-void screen_dislay() {
-  system('clear');
-  for (int i=0;i<H;i++){
-    for (int j=0;j<L;j++){
-      cout << screen [i][j];
-
-
-}
-
-/******************************************************************************/
-
-/* Mettre votre code source ici */
-int main() {
   
-  int key;
+  for(int i=0; i<H; i++)
+    {
+      for (int j=0; j<L; j++)
+	cout << screen[i][j];
+      cout << endl;
+    }
+  // barre
+  if (c==1)
+    for(int j=bg; j<=bd; j++)
+      screen[H-2][j]='=';
 
-  int n=20;
-    do {
-      system("clear");
-      for (int i=0;i<n; i++)
-	cout << endl;
-      cout << " Bonjour Tout Le Monde! " << endl;
-      if (descente)
-	n=(n+1)%20;
-      else {
-	n=n-1;
-	  if (n<=0) n=20;
-      }
-      
-      key = read_keybord();
-      usleep (1000 * 1000);
-      if (key == 'a');
-      descente = true;
-      if (key == 's');
-      descente = false;
-      usleep(10 * 1000);
-    }while ( key !='d');
+
+  
 }
-
 
 /******************************************************************************/
 
+void clear_screen()
+{
+  for (int i=0; i<H; i++)
+    for (int j=0; j<L; j++)
+      if (screen[i][j]=='=') continue;
+      else screen[i][j]= ' ';
+}
+/******************************************************************************/
+int x=1,y=1;
+ int a=1,b=1;
+void update_game(int key)
+
+{
+  if (a==1) // le ballon mobilise de gauche a droite
+    {
+      if (b==1)
+	{
+	  screen[x][y]='*';
+	  x++; y++;
+	}
+
+      if (x==H-1) // je fais comme cela pour eviter le cas " Le ballon apparait 2 fois dans le meme temps "
+	{
+	  b=0;
+	  screen[x][y]='*';
+	  x--; y--; 
+	}
+
+      if (b==0)
+	{ 
+	  screen[x][y]='*';
+	  x--; y++;
+	}
+
+      if (y==L-1 && b==1)
+	{
+	  a=0;
+	  x--; y--;
+	}
+
+      if (y==L-1 && b==0)
+	{
+	  a=0;
+	  x++;y--;
+	}
+	  
+      if(x==0)
+	{
+	  b=1;
+	  x=x+2;
+	}
+      if (screen[x][y]=='=')
+	{
+	  b=0;
+	  x=x-2;
+	}
+    }
+
+  if (a==0)
+    {  
+      if (b==0)
+	{	  
+	  screen[x][y]='*';
+	  x--;y--;
+	}
+
+      if (x==0)
+	{
+	  b=1;
+	  x++;y++;
+	}
+      
+      if (b==1)
+	{
+	  screen[x][y]='*';
+	  x++;y--;
+	}
+      
+      if(x==H-1)
+	{
+	  b=0;
+	  x=x-2;
+	}
+      
+      if(y==0)
+	{
+	  a=1;
+	  y=y+2;
+	}
+      if (screen[x][y]=='=')
+	{
+	  b=0;
+	  x=x-2;
+	  
+	  
+	}
+    }
+
+  
+}
+
+/******************************************************************************/
+void keyProcess(char key)
+{
+  if (key=='a' && bg>=1)   // le barre va a gauche
+    {
+      c=0;
+      screen[H-2][bg]='=';bg--;
+      screen[H-2][bd]=' ';bd--;
+    } 
+  
+  if (key=='d' && bd<=L-2)  // le barre va a droite
+    {
+      c=0; 
+      screen[H-2][bg]=' ';bg++;
+      screen[H-2][bd]='=';bd++;
+    }
+  
+}
+/******************************************************************************/
+int main()
+{
+  char key;   
+  do
+    {
+      key=read_keybord();    
+      clear_screen();
+      update_game(key);
+      screen_display();
+      usleep(75 * 1000);
+      keyProcess(key);      
+    }
+  
+  while (key!= 'q');
+}
+/******************************************************************************/
 int kbhit(void)
 {
   struct termios oldt, newt;
@@ -76,7 +194,7 @@ int kbhit(void)
 
   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
   fcntl(STDIN_FILENO, F_SETFL, oldf);
-
+  
   if(ch != EOF)
   {
     ungetc(ch, stdin);
@@ -86,7 +204,8 @@ int kbhit(void)
   return 0;
 }
 
-int read_keybord() {
+int read_keybord() 
+{
   int key;
   if (kbhit())
     key = getc(stdin);
@@ -94,6 +213,3 @@ int read_keybord() {
     key = 0;
   return key;
 }
-
-/******************************************************************************/
-/******************************************************************************/
